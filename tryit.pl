9a46f89187a91bebeb320f8f1300fa58bd1f7ac7
dhanashri <pdhanu2008@gmail.com>
2020-06-16 09:24:05 +0530

    Added two more files

---
+++ tryit.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+#This is my perl program
+print "Hello,Perl\n"; #This prints Hello Perl
+exit 0;
---
+++ tryit1.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+#This is try it excersise 2
+$sentence = "This\tis\ta\ttest\tmessage";
+print "$sentence\n";
