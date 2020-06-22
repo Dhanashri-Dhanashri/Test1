d7cc4853bfbdc8acc5a032c8f6ad4b2ac5c4f13a
dhanashri <pdhanu2008@gmail.com>
2020-06-22 11:53:48 +0530

    Clean Code on variables and scope

--- testing.pl
+++ testing.pl
@@ -1,13 +1,17 @@
-#use strict;
+use strict;
 use warnings;
 use 5.010;
 use Data::Dumper qw(Dumper);
 use Getopt::Long qw(GetOptions);
 
+my $product;
+my $feature;
+my $from;
 GetOptions('product=s'=>\$product,
 			'feature=s'=>\$feature,
-			'filepath=s'=>\$filepath);
-if(defined $filepath){
+			'from=s'=>\$from);
+			
+if(defined $from){
 	gethash();
 	if(defined $product)
 	{
@@ -15,7 +19,7 @@ if(defined $filepath){
 	}
 	elsif(defined $feature)
 	{
-		getprod($feature);
+		getproduct($feature);
 	}
 }
 else{print "Couldnt find options\n";}
@@ -23,47 +27,44 @@ else{print "Couldnt find options\n";}
 my %hash=();
 #subroutine to get hash
 sub gethash{
-		$filename = $filepath;
-		open(FILE, '<:encoding(UTF-8)',$filename)
-			or die "Could not open the file:$!\n";
-		my $feature;
-		while($line=<FILE>)
-		{	
-			chomp $line;
-			
-			 @product=$line=~m/product=[a-zA-Z0-9-]+/g;	
-			 @feature=$line=~/\b^INCREMENT\s[a-zA-Z0-9]*\b/g;
-			 foreach $s (@feature)
-			{
-				@final = split(/\s+/,$s);
-				next if ($final[1] eq "");
-				$hash{$final[1]} = () unless (defined $hash{$final[1]});
-				$feature = $final[1];
-			}
-			
-		foreach $an (@product)
-			{
-				my @final_product;
-				@final_product = split(/=+/,$an);
-			push @{$hash{$feature}},$final_product[1]  unless ($feature eq '');
-			}
+	my $filename = $from;
+	open(FILE, '<:encoding(UTF-8)',$filename)
+		or die "Could not open the file:$!\n";
+	my $feature;
+	while(my $line=<FILE>)
+	{	
+		chomp $line;
+		 my @productpattern=$line=~m/product=[a-zA-Z0-9-]+/g;	
+		 my @featurepattern=$line=~/\b^INCREMENT\s[a-zA-Z0-9]*\b/g;
+		 foreach my $s (@featurepattern)
+		{
+			my @finalfeaturearray = split(/\s+/,$s);
+			next if ($finalfeaturearray[1] eq "");
+			$hash{$finalfeaturearray[1]} = () unless (defined $hash{$finalfeaturearray[1]});
+			$feature = $finalfeaturearray[1];
 		}
-		close FILE;
-	return Dumper(\%hash);
-	
-	
+			
+		foreach my $p (@productpattern)
+		{
+			my @finalproductarray;
+			@finalproductarray = split(/=+/,$p);
+			push @{$hash{$feature}},$finalproductarray[1]  unless ($feature eq '');
+		}
+	}
+	close FILE;
+	return Dumper(\%hash);	
 }
 
 #subroutine to get product from feature	
-sub getprod
+sub getproduct
 {
 	print "Getting product\n";
-	my $featparam= shift;
-	print "The feature parameter is:".$featparam."\n";
+	my $inputfeature= shift;
+	print "The feature parameter is:".$inputfeature."\n";
 	my @keys= keys %hash;
-	foreach $k (@keys)
+	foreach my $k (@keys)
 	{
-		if ($k eq $featparam)
+		if ($k eq $inputfeature)
 		{
 			print "The product with given feature is:",@{$hash{$k}},"\n";
 		}
@@ -75,14 +76,14 @@ sub getprod
 sub getfeature
 {
 	print "Getting feature\n";
-	my $prodparam=shift;
-	print "The product parameter is:",$prodparam,"\n";
-	while(($key,$value)= each %hash)
+	my $inputproduct=shift;
+	print "The product parameter is:",$inputproduct,"\n";
+	while(my ($key,$value)= each %hash)
 	{
-		@vale=@{$value};
-		foreach $x (@{vale})
+		my @values=@{$value};
+		foreach my $x (@values)
 		{
-			if($x eq $prodparam)
+			if($x eq $inputproduct)
 			{
 				print "The feature of the given product is:",$key,"\n";
 			}
