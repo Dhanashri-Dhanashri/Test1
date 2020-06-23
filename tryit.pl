54eff59232fab46cb86b6f7a81a09abd325850d0
dhanashri <pdhanu2008@gmail.com>
2020-06-23 13:49:05 +0530

    Removed unwanted files

--- tryit.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-#This is my perl program
-print "Hello,Perl\n"; #This prints Hello Perl
-exit 0;
--- tryit1.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-#This is try it excersise 2
-$sentence = "This\tis\ta\ttest\tmessage";
-print "$sentence\n";
